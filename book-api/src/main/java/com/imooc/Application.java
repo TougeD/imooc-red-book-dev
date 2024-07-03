3ce87c414bea0fa723bfc00a9f26ba9cf6849154
Administrator <1844301051@qq.com>
2024-07-03 22:54:33 +0800

    抖音APP初始代码

--- book-api/src/main/java/com/imooc/Application.java
+++ book-api/src/main/java/com/imooc/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author 小小低头哥
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication(exclude = MongoAutoConfiguration.class)
 @Transactional
+@MapperScan(basePackages = "com.imooc.mapper")
 public class Application {
 
     public static void main(String[] args) {
--- book-api/src/main/java/com/imooc/controller/HelloController.java
+++ book-api/src/main/java/com/imooc/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.imooc.controller;
 
+import com.imooc.grace.result.GraceJSONResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@Api(tags = "Hello 测试的接口")
 public class HelloController {
 
     @GetMapping("hello")
+    @ApiOperation("这是一个测试hello的测试路由")
     public Object hello(){
-        return "Hello SpringBoot~";
+        return GraceJSONResult.ok();
     }
 
 }
